d781e9871d70f4f0caa9ebdd23bf9157f8e3cb01
Victoria Jury <you@example.com>
2025-07-25 05:35:35 -0400

    add dispatch to ProductList

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
+    const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});

7ad8c4dc8db24fa7a8c310fbb01615c92e6b779f
Victoria Jury <you@example.com>
2025-07-25 07:10:51 -0400

    Modify button to enable add to cart upon delete

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,7 +4,7 @@ import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
-    const cart = useSelector(state => state.cart.items);
+    const cartItems = useSelector(state => state.cart.items);
     const dispatch = useDispatch();
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -286,7 +286,7 @@ function ProductList({ onHomeClick }) {
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
                         <h1 className='cart'>
-                            <span className='cart_quantity_count'>{cart.reduce((sum, item) => sum + item.quantity, 0)}</span>
+                            <span className='cart_quantity_count'>{cartItems ? cartItems.reduce((sum, item) => sum + item.quantity, 0) : 0}</span>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
                         </h1>
                     </a></div>
@@ -311,19 +311,19 @@ function ProductList({ onHomeClick }) {
                         {/* Display other plant details like description and cost */}
                         <div className="product-description">{plant.description}</div> {/* Display plant description */}
                         <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
-                        {!addedToCart[plant.name] ?
-                        <button
-                            className="product-button"
-                            onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
-                        >
-                            Add to Cart
-                        </button>
-                        :
+                        {cartItems.find(item => item.name === plant.name) ?
                         <button
                             className="product-button added-to-cart"
                         >
                             Added to cart
                         </button>
+                        :
+                        <button
+                            className="product-button"
+                            onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
+                        >
+                            Add to Cart
+                        </button>                        
                         }
                         </div>
                     ))}
